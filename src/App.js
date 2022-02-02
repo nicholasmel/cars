0e81b132759122ac3a503585bd82af8094a10593
Nico Mel <nmel1@asu.edu>
2022-02-02 13:53:09 -0700

    update card component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Card />
+      <Card image={'https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2022/gr-supra/2/2370/a61/1.png?fm=png&bg=white&w=768&h=328&q=90'} title={'2022 Toyota GR Supra'} model={'3.0 Premium'} miles={'7,735 miles'} price={'$55,990'} monthly={'Est. $900/mo'} />
+      <Card image={'https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2022/gr-supra/a91cfedition/2378/b22/1.png?fm=png&bg=white&w=768&h=328&q=90'} title={'2022 Toyota GR Supra'} model={'3.0 Premium'} miles={'7,735 miles'} price={'$55,990'} monthly={'Est. $900/mo'} />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import "../styles/Card.css";
 
-function Card() {
+function Card({ image, title, model, miles, price, monthly }) {
     return (
         <div className='card'>
-            <img src='https://www.toyota.com/imgix/content/dam/toyota/jellies/max/2022/gr-supra/a91cfedition/2378/a99/2.png?fm=png&bg=white&w=768&h=328&q=90' alt='' />
+            <div className='card--border' />
+            <img src={image} alt='' />
             <div className='card--info'>
-                <h1>2022 Toyota GR Supra</h1>
+                <h1>{title}</h1>
                 <div className='card--info-primary'>
-                    <p>3.0 Premium</p>
-                    <p>4,3923 miles</p>
+                    <p>{model}</p>
+                    <p>{miles}</p>
                 </div>
             </div>
             <div className='card--price'>
-                <h1>$55,990</h1>
-                <h2>Est. $890/mo</h2>
+                <h1>{price}</h1>
+                <h2>{monthly}</h2>
             </div>
             <div className='card--button'>
-                <h1>Button</h1>
+                <h1>Purchase</h1>
             </div>
         </div >
     );
