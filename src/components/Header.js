ad370e1066c07c541e262d9ffa86df5ebe2b1ea7
Nico Mel <nmel1@asu.edu>
2022-02-03 02:10:03 -0700

    implement image slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import '../styles/Header.css'
+import React, { useState } from 'react';
+import '../styles/Header.css';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import { sliderdata } from '../sliderdata';
+
 
 function Header() {
+    const [slide, setSlide] = useState(0);
+
+    const nextSlide = () => {
+        setSlide(slide === sliderdata.length - 1 ? 0 : slide + 1);
+    }
+
+    const prevSlide = () => {
+        setSlide(slide === 0 ? sliderdata.length - 1 : slide - 1);
+    }
+
+    if (!Array.isArray(sliderdata) || sliderdata.length <= 0) {
+        return null;
+    }
+
     return (
         <div className="header">
-            <img src="https://toyota.scene7.com/is/image/toyota/SUP_MY22_0012_V001_welcome?fmt=jpg&fit=crop&qlt=90&hei=660" alt="" />
+            {/* <img src="https://toyota.scene7.com/is/image/toyota/SUP_MY22_0012_V001_welcome?fmt=jpg&fit=crop&qlt=90&hei=660" alt="" /> */}
+            <div className='arrow'>
+                <div className='arrow-left' onClick={prevSlide}>
+                    <ArrowCircleLeftIcon sx={{ fontSize: '70px', color: 'white' }} />
+                </div>
+                <div className='arrow-right' onClick={nextSlide}>
+                    <ArrowCircleRightIcon sx={{ fontSize: '70px', color: 'white' }} />
+                </div>
+            </div>
+            {sliderdata.map((item, id) => {
+                return (
+                    <div key={id} className='slider'>
+                        {id === slide && (
+                            <img src={item.img} alt="" />
+                        )}
+                    </div>
+                )
+            }
+            )}
         </div>
     );
 }
---
+++ src/sliderdata.js
@@ -0,0 +1,18 @@
+export const sliderdata = [
+    {
+        id: 1,
+        img: 'https://toyota.scene7.com/is/image/toyota/SUP_MY22_0012_V001_welcome?fmt=jpg&fit=crop&qlt=90&hei=660',
+    },
+    {
+        id: 2,
+        img: 'https://toyota.scene7.com/is/image/toyota/TAC_MY22_0001_V001?wid=2000&fmt=jpg&fit=crop',
+    },
+    {
+        id: 3,
+        img: 'https://toyota.scene7.com/is/image/toyota/086_MY22_0002_V001-1?wid=2000&fmt=jpg&fit=crop',
+    },
+    {
+        id: 4,
+        img: 'https://toyota.scene7.com/is/image/toyota/SUP_MY22_0012_V001_welcome?fmt=jpg&fit=crop&qlt=90&hei=660',
+    },
+]
